f591d01ac3c6ab118ca2b5eeb1a27848542d1e91
Minhyeok Park <pmh_only@pmh.codes>
2025-11-09 11:39:34 +0900

    feat(syncrepo): add syncrepo application

---
+++ syncrepo/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SYNC_MODE=${SYNC_MODE:-"normal"}
+INTERVAL=3600
+
+if [[ $SYNC_MODE == "initsync" ]]; then
+  if [[ -f "/data/initsync.lck" ]]; then
+    echo "syncrepo executed in initsync mode but initsync is already finished. exit."
+    exit 0
+  fi
+
+  /app/syncrepo.sh
+  touch /data/initsync.lck
+fi
+
+if [[ $SYNC_MODE == "normal" ]]; then
+  echo "running in normal sync mode"
+  sleep $(($INTERVAL - $(date +%s) % $INTERVAL))
+
+  while true; do
+    sleep $((RANDOM % $INTERVAL))
+
+    /app/syncrepo.sh
+
+    sleep $(($INTERVAL - $(date +%s) % $INTERVAL))
+  done
+fi
