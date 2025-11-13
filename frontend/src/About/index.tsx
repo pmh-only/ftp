503cb8262f46915d8865728d9946225fd6be569c
Minhyeok Park <pmh_only@pmh.codes>
2025-11-13 20:07:57 +0900

    feat(frontend): add how to apply this mirror

--- frontend/src/About/index.tsx
+++ frontend/src/About/index.tsx
@@ -4,18 +4,29 @@ interface Props {
 
 function About({ className }: Props) {
   return (
-    <div className={className + ' overflow-y-auto p-2'}>
+    <div className={className + ' overflow-y-auto p-2 flex gap-6'}>
       <div
-        className="hero bg-base-300 h-full max-h-64 rounded-2xl"
-        style={{ backgroundImage: 'url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)' }}>
-        <div className="hero-overlay"></div>
+        className="hero bg-base-300 h-full max-h-42 md:max-h-64 rounded-2xl"
+        style={{ backgroundImage: 'url(/_assets/aboutbg.webp)' }}>
         <div className="flex flex-col justify-end items-start p-6 pb-8 w-full h-full gap-2 italic">
           <h1 className="font-bold italic font-display text-6xl">
             <span className="font-thin">ftp</span>.io.kr.
           </h1>
         </div>
       </div>
-    </div>
+
+      <div className="mockup-code w-full">
+        <pre data-prefix="$"><code>sudo vi /etc/pacman.d/mirrorlist</code></pre>
+        <pre data-prefix="1" className="text-info"><code>##</code></pre>
+        <pre data-prefix="2" className="text-info"><code>## Arch Linux repository mirrorlist</code></pre>
+        <pre data-prefix="3" className="text-info"><code>## Generated on ...</code></pre>
+        <pre data-prefix="4" className="text-info"><code>##</code></pre>
+        <pre data-prefix="5"><code></code></pre>
+        <pre data-prefix="6"><code>Server = http://ftp.io.kr/<b>$repo/os/$arch</b></code> <code className="text-info"># or..</code></pre>
+        <pre data-prefix="7"><code>Server = https://ftp.io.kr/<b>$repo/os/$arch</b></code></pre>
+        <pre data-prefix="8"><code></code></pre>
+      </div>
+    </div >
   )
 }
 
