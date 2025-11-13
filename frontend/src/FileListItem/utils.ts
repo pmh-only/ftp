2f46d0a91ea5c861bfd42f1d4d2a157b2dbc6025
Minhyeok Park <pmh_only@pmh.codes>
2025-11-13 14:31:53 +0900

    feat(frontend): use more various color for last update relative string

--- frontend/src/FileListItem/utils.ts
+++ frontend/src/FileListItem/utils.ts
@@ -10,17 +10,20 @@ export function getLastUpdateRelative(item: FileModel): [string, string] {
   if (lastUpdateRelativeNumber < 1000)
     return ['now', 'text-red-500']
 
-  if (lastUpdateRelativeNumber < 60 * 1000)
-    return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 1000), 'seconds'), 'text-red-500']
-
   if (lastUpdateRelativeNumber < 60 * 60 * 1000)
     return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 60 / 1000), 'minutes'), 'text-red-500']
 
-  if (lastUpdateRelativeNumber < 24 * 60 * 60 * 1000)
+  if (lastUpdateRelativeNumber < 5 * 60 * 60 * 1000)
     return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 60 / 60 / 1000), 'hours'), 'text-red-500']
 
+  if (lastUpdateRelativeNumber < 24 * 60 * 60 * 1000)
+    return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 60 / 60 / 1000), 'hours'), 'text-orange-500']
+
+  if (lastUpdateRelativeNumber < 7 * 24 * 60 * 60 * 1000)
+    return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 24 / 60 / 60 / 1000), 'days'), 'text-amber-500']
+
   if (lastUpdateRelativeNumber < 30 * 24 * 60 * 60 * 1000)
-    return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 24 / 60 / 60 / 1000), 'days'), 'text-orange-500']
+    return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 24 / 60 / 60 / 1000), 'days'), 'text-yellow-500']
 
   return [rtf.format(-Math.floor(lastUpdateRelativeNumber / 24 / 60 / 60 / 1000), 'days'), 'text-neutral-500']
 }
